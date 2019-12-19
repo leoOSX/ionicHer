25bb42d32eea099b0e407630876149c47c7e75d7
leoOSX <agusosx@gmail.com>
2019-12-18 22:51:15 -0400

    *

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './services/auth.service';
+import { AlertService } from './services/alert.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   public appPages = [
+    {
+      title: 'Dashboard',
+      url: '/dashboard',
+      icon: 'home'
+    },
     {
       title: 'Home',
       url: '/home',
@@ -20,13 +26,16 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
-    }
+    },
   ];
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authService: AuthService,
+    private navCtrl: NavController,
+    private alertService: AlertService
   ) {
     this.initializeApp();
   }
@@ -34,7 +43,22 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      //this.splashScreen.hide();
+      this.authService.getToken();
     });
+  }
+
+  logout() {
+    this.authService.logout().subscribe(
+      data => {
+        this.alertService.presentToast(data['message']);        
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        this.navCtrl.navigateRoot('/landing');
+      }
+    );
   }
 }
---
+++ src/app/models/user.ts
@@ -0,0 +1,6 @@
+export class User{
+    id: number;
+    nombre_completo: string;
+    email:string;
+
+}
